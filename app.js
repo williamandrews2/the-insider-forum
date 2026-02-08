46dedc3d1e41811ed5237f47a6181c5050e839c8
William A. <121400607+williamandrews2@users.noreply.github.com>
2026-02-08 12:31:36 -0600

    Create route files and mounted routes in app.js

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+// port variables
+const PORT = process.env.PORT || 3030;
+
+// home route
 app.get("/", (req, res) => {
   res.send("Express is running!");
 });
 
-const PORT = process.env.PORT || 3030;
+// route variables
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const messagesRouter = require("./routes/messages");
 
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/messages", messagesRouter);
+
+// starting the server
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
