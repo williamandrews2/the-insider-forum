706db46460e3422715190cd1f8d11c4d9a63fe9c
William A. <121400607+williamandrews2@users.noreply.github.com>
2026-02-16 21:22:23 -0600

    Updated schema for isAdmin value. Migrated db.

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,11 @@ import { defineConfig, env } from "prisma/config";
 import "dotenv/config";
 
 export default defineConfig({
-  schema: "prisma/prisma.schema",
+  schema: "prisma",
   datasource: {
     url: env("DATABASE_URL"),
+  },
+  migrations: {
+    path: "prisma/migrations",
   },
 });
---
+++ prisma/migrations/20260217031058_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "membershipStatus" BOOLEAN NOT NULL DEFAULT false,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "timeStamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
